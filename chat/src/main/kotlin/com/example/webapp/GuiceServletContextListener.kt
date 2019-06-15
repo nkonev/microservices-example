03d2435686dd4e7c1325426a800ae3bf4509c55a
Nikita Konev <nikit.cpp@yandex.ru>
2019-06-15 16:26:43 +0300

    initial tracing grpc client on java side

--- chat/src/main/kotlin/com/example/webapp/GuiceServletContextListener.kt
+++ chat/src/main/kotlin/com/example/webapp/GuiceServletContextListener.kt
@@ -1,5 +1,7 @@
 package com.example.webapp
 
+import brave.Tracing
+import brave.grpc.GrpcTracing
 import com.codenotfound.grpc.helloworld.HelloServiceGrpc
 import com.example.webapp.repo.ChatRepository
 import com.example.webapp.utils.CONFIG_PREFIX_APP
@@ -12,23 +14,29 @@ import com.mongodb.client.MongoClient
 import com.mongodb.client.MongoClients
 import io.grpc.ManagedChannel
 import io.grpc.ManagedChannelBuilder
-import org.cfg4j.provider.ConfigurationProviderBuilder
-import org.cfg4j.source.classpath.ClasspathConfigurationSource
-import org.cfg4j.source.compose.MergeConfigurationSource
-import org.cfg4j.source.system.EnvironmentVariablesConfigurationSource
 import org.slf4j.LoggerFactory
 import javax.servlet.ServletContextEvent
 import javax.servlet.ServletContextListener
 import javax.servlet.annotation.WebListener
+import zipkin2.reporter.AsyncReporter
+import zipkin2.reporter.Sender
+import javax.servlet.DispatcherType
+import java.util.EnumSet
+import brave.servlet.TracingFilter
+import zipkin2.reporter.okhttp3.OkHttpSender
 
 @WebListener
 class GuiceServletContextListener : ServletContextListener {
 
-    override fun contextInitialized(sce: ServletContextEvent?) {
+    override fun contextInitialized(servletContextEvent: ServletContextEvent?) {
+        servletContextEvent
+                ?.getServletContext()
+                ?.addFilter("tracingFilter", TracingFilter.create(tracing))
+                ?.addMappingForUrlPatterns(EnumSet.allOf(DispatcherType::class.java), true, "/*")
         LOGGER.info("Servlet context initialized")
     }
 
-    override fun contextDestroyed(sce: ServletContextEvent?) {
+    override fun contextDestroyed(servletContextEvent: ServletContextEvent?) {
         close()
         println("Servlet context and logger destroyed")
     }
@@ -39,6 +47,7 @@ class GuiceServletContextListener : ServletContextListener {
         private val config: SampleConfig
         private val mongoClient : MongoClient
         private val LOGGER = LoggerFactory.getLogger(GuiceServletContextListener::class.java)
+        private val tracing: Tracing
         private val grpcClient: ManagedChannel
         private val helloServiceStub: HelloServiceGrpc.HelloServiceBlockingStub
 
@@ -46,9 +55,18 @@ class GuiceServletContextListener : ServletContextListener {
             config = buildConfigurationProvider().bind(CONFIG_PREFIX_APP, SampleConfig::class.java)
             mongoClient = MongoClients.create(config.mongodbUrl())
 
+            val sender: Sender = OkHttpSender.create("http://127.0.0.1:9411/api/v2/spans")
+            val spanReporter = AsyncReporter.create(sender)
+
+            tracing = Tracing.newBuilder()
+                    .localServiceName("chat")
+                    .spanReporter(spanReporter)
+                    .build();
+            val grpcTracing = GrpcTracing.create(tracing);
             // https://codenotfound.com/grpc-java-example.html
             // https://www.baeldung.com/grpc-introduction
             grpcClient = ManagedChannelBuilder.forAddress(config.grpcHost(), config.grpcPort())
+                    .intercept(grpcTracing.newClientInterceptor())
                     .usePlaintext()
                     .build()
             helloServiceStub = HelloServiceGrpc.newBlockingStub(grpcClient);
@@ -68,6 +86,11 @@ class GuiceServletContextListener : ServletContextListener {
             } catch (e: Exception) {
                 println("Error on closing grpc client")
             }
+            try {
+                tracing.close()
+            } catch (e: Exception) {
+                println("Error on closing tracing")
+            }
         }
     }
 
@@ -77,6 +100,7 @@ class GuiceServletContextListener : ServletContextListener {
             bind(SampleConfig::class.java).toInstance(config)
             bind(MongoClient::class.java).toInstance(mongoClient)
             bind(ChatRepository::class.java).asEagerSingleton()
+            bind(Tracing::class.java).toInstance(tracing)
             bind(ManagedChannel::class.java).toInstance(grpcClient)
             bind(HelloServiceGrpc.HelloServiceBlockingStub::class.java).toInstance(helloServiceStub)
         }
